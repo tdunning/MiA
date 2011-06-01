f06c477f5b80f1fa31bb7510b7cb95407db55bc4
robinanil <robinanil@apache.org>
2011-06-02 03:03:09 +0530

    Fix clustering code to conform to latest version of Mahout

---
+++ MiA/src/main/java/mia/clustering/ch09/RandomPointsUtil.java
@@ -0,0 +1,26 @@
+package mia.clustering.ch09;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.mahout.common.RandomUtils;
+import org.apache.mahout.math.Vector;
+
+public class RandomPointsUtil {
+  public static List<Vector> chooseRandomPoints(Iterable<Vector> vectors, int k) {
+    List<Vector> chosenPoints = new ArrayList<Vector>(k);
+    Random random = RandomUtils.getRandom();
+    for (Vector value : vectors) {
+      int currentSize = chosenPoints.size();
+      if (currentSize < k) {
+        chosenPoints.add(value);
+      } else if (random.nextInt(currentSize + 1) == 0) { // with chance 1/(currentSize+1) pick new element
+        int indexToRemove = random.nextInt(currentSize); // evict one chosen randomly
+        chosenPoints.remove(indexToRemove);
+        chosenPoints.add(value);
+      }
+    }
+    return chosenPoints;
+  }
+}
