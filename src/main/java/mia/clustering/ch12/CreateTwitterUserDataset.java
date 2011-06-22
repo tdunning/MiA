0af7b201ab742c611822a72f383d29563d4ff352
Alex Ott <alexott@gmail.com>
2011-06-22 21:01:12 +0200

    add grabber for tweets using twitter4j library
    also made some cleanup of pom.xml

--- src/main/java/mia/clustering/ch12/CreateTwitterUserDataset.java
+++ src/main/java/mia/clustering/ch12/CreateTwitterUserDataset.java
@@ -2,25 +2,25 @@ package mia.clustering.ch12;
 
 import mia.clustering.ch12.twitter.ByKeyGroupingJob;
 
-import org.apache.commons.codec.language.DoubleMetaphone;
+//import org.apache.commons.codec.language.DoubleMetaphone;
 import org.apache.mahout.common.Parameters;
 
 public class CreateTwitterUserDataset {
   public static void main(String args[]) throws Exception {
     Parameters params = new Parameters();
     params.set("splitPattern", "\t");
-    String inputDir = "infochimps_five-days-of-25bahman-tweets/tweets_25bahman.csv";
+    String inputDir = "tweets.txt";
     String outputDir = "twitter_seqfiles";
     params.set("input", inputDir);
     params.set("output", outputDir);
-    params.set("selectedField", "3"); // tweet
-    params.set("groupByField", "2"); // username
+    params.set("selectedField", "1"); // tweet
+    params.set("groupByField", "0"); // username
     ByKeyGroupingJob.startJob(params);
-    DoubleMetaphone filter = new DoubleMetaphone();
-    // TODO: change these terms?!
-    System.out.println(filter.encode("Loke"));
-    System.out.println(filter.encode("companymancomic"));
-    System.out.println(filter.encode("webcomics"));
-    System.out.println(filter.encode("@comic"));
+//    DoubleMetaphone filter = new DoubleMetaphone();
+// TODO: change these terms?!
+//    System.out.println(filter.encode("Loke"));
+//    System.out.println(filter.encode("companymancomic"));
+//    System.out.println(filter.encode("webcomics"));
+//    System.out.println(filter.encode("@comic"));
   }
 }
---
+++ src/main/java/mia/clustering/ch12/TwitterDownloader.java
@@ -0,0 +1,74 @@
+/*
+ * To run this example you need to obtain auth keys from https://dev.twitter.com/apps
+ * and enter them into corresponding .setOAuthXXXX methods in the main function below
+ * 
+ */
+package mia.clustering.ch12;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterDownloader {
+	private class MahoutListener implements StatusListener {
+    	long count = 0;
+    	static final long maxCount = 100000; // change this if you want another number of tweets to collect
+    	PrintWriter out;
+    	TwitterStream tweetStream;
+    	
+    	MahoutListener (TwitterStream ts) throws IOException {
+    		tweetStream = ts;
+    		out = new PrintWriter(new BufferedWriter(new FileWriter("tweets.txt")));
+    	}
+    	
+        public void onStatus(Status status) {
+            out.println(status.getUser().getId() + "\t" + status.getText());
+            System.out.println(status.getUser().getId() + "\t" + status.getText());
+            count++;
+            if(count >= maxCount) {
+            	tweetStream.shutdown();
+            	out.close();
+            }
+        }
+
+        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+        }
+
+        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+        }
+
+        public void onScrubGeo(long userId, long upToStatusId) {
+        }
+
+        public void onException(Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+	
+	StatusListener makeListener(TwitterStream ts) throws IOException {
+		return this.new MahoutListener(ts);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("*********")
+		  .setOAuthConsumerSecret("************")
+		  .setOAuthAccessToken("*******************")
+		  .setOAuthAccessTokenSecret("****************");
+		TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+        TwitterDownloader td = new TwitterDownloader();
+        StatusListener listener = td.makeListener(twitterStream);
+        twitterStream.addListener(listener);
+        twitterStream.sample();
+	}
+
+}
